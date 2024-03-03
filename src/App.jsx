74ccf05ec62281915f97c2406a25a85e4e31a0b3
Erdoğan FIRAT <efirat3@gmail.com>
2024-03-03 19:52:04 +0300

    feat: State örneği yapıldı.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import UrunKart from "./UrunKart"
+import { useState } from "react";
+
 function App(){
+  const [ad, adGuncelle] = useState("Erdoğan")
+  console.log("app çalıştı")
+
   return(
-    <div className="container">
-      <h2>Ürün Listesi</h2>
-      <div className="row">
-        <UrunKart urunBilgi={{baslik:"Ürün Başlığı 1", fotoNo:12}} />
-        <UrunKart urunBilgi={{baslik:"Ürün Başlığı 2", fotoNo:22}} />
-        <UrunKart urunBilgi={{baslik:"Ürün Başlığı 3", fotoNo:32}} />
-        <UrunKart urunBilgi={{baslik:"Ürün Başlığı 4", fotoNo:42}} />
-        <UrunKart />
-        
-      </div>
+    <div>
+    Merhaba! {ad}
+    <button onClick={ () => {adGuncelle("Gonca")}}>Adı Değiştir</button>
     </div>
   )
 }
